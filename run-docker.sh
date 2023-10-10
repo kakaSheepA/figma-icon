32210dd22642d5821e600605fe0f80bd000af901
Kaka_Ka <1023454254@qq.com>
2023-10-10 18:44:16 +0800

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -2,3 +2,15 @@
 yarn
 yarn build:git-extractor
 yarn dev
+  "include": ["src/**/*.ts", "typings/**/*.ts"],
+  "exclude": ["node_modules", "__tests__", "build", "**/*.test.ts", "temp"]
+    "include": ["src/**/*.ts", "typings/**/*.ts"],
+  "exclude": ["node_modules", "__tests__", "build", "**/*.test.ts", "temp"]
+    "include": ["src/**/*.ts", "typings/**/*.ts"],
+  "exclude": ["node_modules", "__tests__", "build", "**/*.test.ts", "temp"]
+    "include": ["src/**/*.ts", "typings/**/*.ts"],
+  "exclude": ["node_modules", "__tests__", "build", "**/*.test.ts", "temp"]
+    "include": ["src/**/*.ts", "typings/**/*.ts"],
+  "exclude": ["node_modules", "__tests__", "build", "**/*.test.ts", "temp"]
+    "include": ["src/**/*.ts", "typings/**/*.ts"],
+  "exclude": ["node_modules", "__tests__", "build", "**/*.test.ts", "temp"]
